4106caf33fdf4dacf23987b76d8b4187380957b2
james <jtwebco@gmail.com>
2021-03-01 11:50:14 -0500

    remove nrdstr

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,6 @@ const Footer = ({ auth }) => {
   return (
     <div className="text footer__container">
       <p>&copy; Drafty 2020</p>
-      <p className='footer__powered-by'>Powered by &nbsp; <a href='https://nrdstr.com' target='nrdstr.com' target='_blank' rel='nofollow'><span className='footer__nrdstr-logo' /></a></p>
     </div>
   )
 }
